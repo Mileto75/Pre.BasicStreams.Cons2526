6a973b2f5bd955452c62a225ef248b5e268ac206
Mileto Di Marco <mileto.di.marco@howest.be>
2026-03-17 13:38:51 +0100

    Add project files.

---
+++ Pre.BasicStreams.Cons/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Basic streams");
+var pathToDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Pre", "Secrets");
+//check if directory exists
+if(!Directory.Exists(pathToDirectory))
+{
+    Directory.CreateDirectory(pathToDirectory);
+}
+var fullPathToFile = Path.Combine(pathToDirectory, "Users.csv");
+//check if file exists
+Console.WriteLine(pathToDirectory);
+if(!File.Exists(fullPathToFile))
+{
+    File.Create(fullPathToFile);
+}
+try
+{
+    //open a stream = file is locked by operating system
+    FileStream fileStream = new FileStream(fullPathToFile, FileMode.Open);
+    var charByte = fileStream.ReadByte();
+    while(charByte != -1)
+    {
+        Console.Write((char)charByte);
+        charByte = fileStream.ReadByte();
+    }
+    fileStream.Dispose();
+}
+catch(FileNotFoundException fileNotFoundException)
+{
+    Console.WriteLine(fileNotFoundException.Message);
+}
+
+StreamReader streamReader = new StreamReader(fullPathToFile);
+Console.WriteLine(streamReader.ReadToEnd());
